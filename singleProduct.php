9d64c0d28ba458f571f42ce3065b7833af72717f
khozamaObeidat <100359224+khozamaObeidat@users.noreply.github.com>
2022-05-19 01:15:54 +0300

    cartPlussingleAdded

---
+++ singleProduct.php
@@ -0,0 +1,103 @@
+<?php
+include_once "header.php"
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="singleP.css">
+  </head>
+  <body>
+  <main class="container">
+ 
+ <!-- Left Column /   Image -->
+ <div class="left-column">
+   
+   <img data-image="red" class="active" src="fwy6zosqphc8hzjk0rgr.webp" alt="">  
+ </div>
+
+
+ <!-- Right Column -->
+ <div class="right-column">
+
+   <!-- Product Description -->
+   <div class="product-description">
+     <span>Product title</span>
+     <h1>Product name</h1>
+     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime dolore quaerat, in corporis ab error ea dolorum ullam voluptatem repellat totam rem enim a eius harum vitae minus repellendus sequi.</p>
+   </div>
+
+   <!-- Product Configuration -->
+   <div class="product-configuration">
+
+     <!-- Product Color -->
+     <!-- <div class="product-color">
+       <span>Color</span>
+
+       <div class="color-choose">
+         <div>
+           <input data-image="red" type="radio" id="red" name="color" value="red" checked>
+           <label for="red"><span></span></label>
+         </div>
+         <div>
+           <input data-image="blue" type="radio" id="blue" name="color" value="blue">
+           <label for="blue"><span></span></label>
+         </div>
+         <div>
+           <input data-image="black" type="radio" id="black" name="color" value="black">
+           <label for="black"><span></span></label>
+         </div>
+       </div>
+
+     </div> -->
+
+     <!-- Cable Configuration -->
+     <div class="cable-config">
+       <span>Options</span>
+
+       <div class="cable-choose">
+         <button>option1</button>
+         <button>option2</button>
+         <button>option3</button>
+       </div>
+
+       <small style="color:gray ;">Make Sure You Chose a Size Or Color Before Adding To The Cart</small>
+     </div>
+   </div>
+
+   <!-- Product Pricing -->
+   <div class="product-price">
+     <span>$price</span>
+     <a href="#" class="cart-btn">Add to cart</a>
+   </div>
+ </div>
+</main>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+    <footer class="footer text-center text-white" style="background-color: rgb(26, 26, 26) ;">
+    
+  
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(70, 70, 70, 0.907);">
+      © 2022 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">FocusZone.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  </body>
+</html>
+ 
