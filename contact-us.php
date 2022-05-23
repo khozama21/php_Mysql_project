322d2a5dc0368a1da10ce557ebd85d6cb6cd407d
khozamaObeidat <100359224+khozamaObeidat@users.noreply.github.com>
2022-05-23 22:55:51 +0300

    conatctus_Edit

--- contact-us.php
+++ contact-us.php
@@ -71,12 +71,12 @@ include_once "../php_Mysql_project/headFoot/header.php";
 	    			<div class="contact-info">
 	    				<h2 class="title text-center">Contact Info</h2>
 	    				<address>
-	    					<p>E-Shopper Inc.</p>
-							<p>935 W. Webster Ave New Streets Chicago, IL 60614, NY</p>
-							<p>Newyork USA</p>
+	    					<p>Focus Zone Inc.</p>
+							<!-- <p>935 W. Webster Ave New Streets Chicago, IL 60614, NY</p> -->
+							<p>Amman Jordan</p>
 							<p>Mobile: +2346 17 38 93</p>
-							<p>Fax: 1-714-252-0026</p>
-							<p>Email: info@e-shopper.com</p>
+							<!-- <p>Fax: 1-714-252-0026</p> -->
+							<p>Email: info@focuszone.com</p>
 	    				</address>
 	    				<div class="social-networks">
 	    					<h2 class="title text-center">Social Networking</h2>
